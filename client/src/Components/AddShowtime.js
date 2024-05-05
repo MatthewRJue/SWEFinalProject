94271fc4fc98585964ad625dd3d3de0b4eaa98b4
MatthewRJue <matthewjue101@gmail.com>
2024-05-05 18:21:36 -0400

    added seat list

--- client/src/Components/AddShowtime.js
+++ client/src/Components/AddShowtime.js
@@ -9,7 +9,10 @@ const AddShowtime = ({isOpen, onClose, movieList, showtimeList}) => {
     const [date, setDate] = useState("");
     const [time, setTime] = useState("");
     const [movieid, setID] = useState("")
-  
+    const [seatList, setSeatList] = useState({A1: "",A2: "",A3: "",A4: "",A5: "",A6: "",A7: "",A8: "",A9: "",A10: "",A11: "", A12: "",
+  B1: "",B2: "",B3: "",B4: "",B5: "",B6: "",B7: "",B8: "",B9: "",B10: "",B11: "",B12: "",
+C1: "",C2: "",C3: "",C4: "",C5: "",C6: "",C7: "",C8: "",C9: "",C10: "",C11: "",C12: ""})
+
     const handleSubmit = async (event) => {
         event.preventDefault();
 
@@ -47,6 +50,7 @@ const AddShowtime = ({isOpen, onClose, movieList, showtimeList}) => {
         room,
         date,
         time,
+        seatList
     }
    
     
